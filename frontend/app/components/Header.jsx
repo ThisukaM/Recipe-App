a240db2f0e2ab410faa7ea1eb61b4d402e6216c4
nlia656 <nlia656@aucklanduni.ac.nz>
2024-10-07 23:17:44 +1300

    implemented hamburger menu

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -1,32 +1,46 @@
 "use client";
 
 import Link from 'next/link';
-import { Button } from "@nextui-org/react";
+import './header.css';
+import { useRef } from 'react';
 
 export default function Header() {
+  const savedOptionRef = useRef(null);
+  const aboutOptionRef = useRef(null);
+  const hamburgerClick = () => {
+    if (savedOptionRef.current && aboutOptionRef.current) {
+      savedOptionRef.current.style.display = savedOptionRef.current.style.display === "block" ? "none" : "block";
+      aboutOptionRef.current.style.display = aboutOptionRef.current.style.display === "block" ? "none" : "block";
+    }    
+  }
+
   return (
-    <div className="bg-white py-4 px-6 flex justify-between items-center">
-      <Link
-        href="/"
-        passHref
-        className="text-xl md:text-4xl font-bold text-purple-700 cursor-pointer">
+    <div>
+        <div className="bg-purple-700 py-4 px-6 flex justify-between items-center">
+        <Link
+          href="/"
+          passHref
+          className="text-xl md:text-4xl font-bold text-white cursor-pointer">
   
-          MEALMATCH
+            MEALMATCH
         
-      </Link>
-      <div className="inline-block cursor-pointer md:hidden">
-        <div className="w-8 h-1 bg-purple-700 my-1.5"></div>
-        <div className="w-8 h-1 bg-purple-700 my-1.5"></div>
-        <div className="w-8 h-1 bg-purple-700 my-1.5"></div>
+        </Link>
+        <button id="hamburger-button" className="inline-block cursor-pointer md:block" onClick={hamburgerClick}>
+          <div className="w-8 h-1 bg-white my-1.5"></div>
+          <div className="w-8 h-1 bg-white my-1.5"></div>
+          <div className="w-8 h-1 bg-white my-1.5"></div>
+        </button>
+        <div className="space-x-4 hidden md:block">
+          <button className="bg-purple-700 px-5 py-2.5 border-2 border-color-white text-white rounded-xl hover:bg-white hover:text-purple-700">
+            Saved Recipes
+          </button>
+          <button className="bg-purple-700 px-5 py-2.5 border-2 border-color-white text-white rounded-xl hover:bg-white hover:text-purple-700">
+            About
+          </button>
+        </div>
       </div>
-      <div className="space-x-4 hidden md:block">
-        <Button color="primary" variant="ghost" size="lg">
-          Saved Recipes
-        </Button>
-        <Button color="primary" variant="ghost" size="lg">
-          About
-        </Button>
-      </div>
+      <div id="saved-option" ref={savedOptionRef} style={{ display: 'none' }} className="w-full bg-purple-400 text-md py-3 px-6 text-white">Saved Recipes</div>
+      <div id="about-option" ref={aboutOptionRef} style={{ display: 'none' }} className="w-full bg-purple-400 text-md py-3 px-6 text-white">About</div>
     </div>
   );
 }
