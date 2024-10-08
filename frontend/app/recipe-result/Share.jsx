0ff08d9640117888a6c6e5afa866e16368b32da2
Thisuka <tmat871@aucklanduni.ac.nz>
2024-10-09 02:23:26 +1300

    Fix: Fixed layout of social sharing buttons and made twitter link open in a new tab.

--- frontend/app/recipe-result/Share.jsx
+++ frontend/app/recipe-result/Share.jsx
@@ -1,41 +1,21 @@
 import { CardBody } from "@nextui-org/react";
-import React, { useEffect } from 'react';
-
 
 export default function Share({ instructions, ingredients, title, recipeId }) {
+
+    
     const instructionList = Array.isArray(instructions) ? instructions : [];
     const ingredientsList = Array.isArray(ingredients) ? ingredients : [];
-
-    // useEffect(() => {
-    //     // Load the Twitter widgets script after the component mounts
-    //     const script = document.createElement('script');
-    //     script.src = 'https://platform.twitter.com/widgets.js';
-    //     script.async = true;
-    //     script.charset = 'utf-8';
-    //     document.body.appendChild(script);
-    // }, []);
 
     let instructionsString ="Ingredients: \n";
     let tweetString = "";
 
-    for(let i = 0; i<ingredientsList.length;i++){
-        instructionsString = instructionsString.concat("- " + ingredientsList[i] + "\n")
+    for(const element of ingredientsList){
+        instructionsString = instructionsString.concat("- " + element + "\n")
     }
     instructionsString = instructionsString.concat("\n Cooking Instructions: \n")
 
     for(let i = 0; i<instructionList.length;i++){
         instructionsString = instructionsString.concat((i+1) + ". " + instructionList[i] + "\n")
-    }
-
-    for(let i = 0;i<instructionsString.length;i++){
-        let currentChar = instructionsString[i];
-        // if(currentChar == " "){
-        //     currentChar = "+";
-        // }
-        // if(currentChar == "\n"){
-        //     currentChar = "%0A";
-        // }
-        tweetString = tweetString.concat(currentChar);
     }
 
     console.log("LOL " + tweetString);
@@ -48,24 +28,27 @@ export default function Share({ instructions, ingredients, title, recipeId }) {
         <CardBody className="p-4">
         <div className="flex items-center mb-2 gap-4">
             <a 
-                className="flex items-center gap-2 px-4 py-2 rounded-md text-white bg-indigo-500 hover:bg-indigo-600 transition-colors" 
+                className="flex items-center gap-2 px-10 py-2 w-full rounded-md text-white bg-indigo-500 hover:bg-indigo-600 transition-colors" 
                 href={emailLink} 
                 target="_blank" 
                 rel="noopener noreferrer"
+                style={{
+                    paddingLeft: '8px',
+                    paddingRight: '20px'
+                }}
             >
-                <span role="img" aria-label="Email">✉️</span> Email
+            <span aria-label="Email">✉️</span> Email
             </a>
             <a 
-                className="twitter-share-button flex items-left gap-2 px-6 py-2 rounded-md text-white bg-black hover:bg-black transition-colors" 
+                className="twitter-share-button flex items-center gap-2 px-6 py-2 rounded-md text-white bg-black hover:bg-black transition-colors" 
                 href={twitterLink} 
-                data-size="large"
+                target="_blank" 
                 style={{
                     paddingLeft: '8px',
                     paddingRight: '32px'
                 }}
             >
-                <img src="/X_logo_2023_(white).png" alt="facebook Icon" className="h-5 w-5" />
-                Tweet
+                <img src="/X_logo_2023_(white).png" alt="facebook Icon" className="h-5 w-5" />Tweet
             </a>
             <a 
                 className="flex items-center gap-2 px-8 py-2 w-full rounded-md text-white bg-blue-600 hover:bg-blue-700 transition-colors" 
@@ -77,14 +60,12 @@ export default function Share({ instructions, ingredients, title, recipeId }) {
                     paddingRight: '32px'
                 }}
             >
-                <img src="/Facebook_Logo_2023.png" alt="facebook Icon" className="h-5 w-5" />
-                Facebook
+                <img src="/Facebook_Logo_2023.png" alt="facebook Icon" className="h-5 w-5" />Facebook
             </a>
         </div>
     </CardBody>
     );
 }
-
 
 const styles = {
     emailButton: {
